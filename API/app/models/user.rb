30c513dc8f7d324f7eb3f6566afa3f96a657a47b
luisbarajasp <luisbarajasp@gmail.com>
2018-01-18 18:49:06 -0600

    Add: Paperclip to user

--- API/app/models/user.rb
+++ API/app/models/user.rb
@@ -1,6 +1,7 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -50,9 +51,25 @@ class User
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
+  ## Paperclip
+  has_mongoid_attached_file :avatar,
+    :path           => ':attachment/:id/:style.:extension',
+    # :storage        => :s3,
+    # :url            => ':s3_alias_url',
+    # :s3_host_alias  => 'something.cloudfront.net',
+    # :s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
+    :styles => {
+      :original => ['1920x1680>', :jpg],
+      :small    => ['100x100#',   :jpg],
+      :medium   => ['250x250',    :jpg],
+      :large    => ['500x500>',   :jpg]
+    },
+    # :convert_options => { :all => '-background white -flatten +matte' }
+
   def self.find_by_login(login)
     where(:username => login).first || where(:email => login).first
   end
+  
   def all_friendships
     all_friendships = []
     all_friendships << friendships
